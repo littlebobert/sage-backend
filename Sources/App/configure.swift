91d59771b9ce8b1b4e812454492745afc7e77213
Justin Garcia <justin.garcia@gmail.com>
2019-04-21 11:24:36 -0700

    try db config stuff again

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -8,15 +8,40 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     try services.register(FluentMySQLProvider())
     try services.register(LeafProvider())
     
-    let mysqlConfig = MySQLDatabaseConfig(
-        hostname: Environment.get("$DATABASE_HOSTNAME")!,
-        port: Int(Environment.get("$DATABASE_PORT")!)!,
-        username: Environment.get("$DATABASE_USER")!,
-        password: Environment.get("$DATABASE_PASSWORD")!,
-        database: Environment.get("$DATABASE_DB")!,
-        transport: MySQLTransportConfig.unverifiedTLS
-    )
-    services.register(mysqlConfig)
+    let databaseConfig: MySQLDatabaseConfig
+    
+    if let url = Environment.get("DATABASE_URL") {
+        databaseConfig = try! MySQLDatabaseConfig(url: url)!
+    } else if let url = Environment.get("DB_MYSQL") {
+        databaseConfig = try! MySQLDatabaseConfig(url: url)!
+    } else {
+        let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+        let username = Environment.get("DATABASE_USER") ?? "vapor"
+        let password = Environment.get("DATABASE_PASSWORD") ?? "password"
+        let databaseName: String
+        let databasePort: Int
+        if (env == .testing) {
+            databaseName = "vapor-test"
+            if let testPort = Environment.get("DATABASE_PORT") {
+                databasePort = Int(testPort) ?? 5433
+            } else {
+                databasePort = 5433
+            }
+        } else {
+            databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+            databasePort = 5432
+        }
+        
+        databaseConfig = MySQLDatabaseConfig(
+            hostname: hostname,
+            port: databasePort,
+            username: username,
+            password: password,
+            database: databaseName,
+            transport: .unverifiedTLS
+        )
+    }
+    services.register(databaseConfig)
     
     var migrations = MigrationConfig()
     migrations.add(model: User.self, database: .mysql)
